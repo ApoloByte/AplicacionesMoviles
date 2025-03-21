36c0fdec3c30e86ed592ee105b869fd29b0acefd
Victor-P <victorpinto2304@gmail.com>
2025-03-21 08:48:07 -0500

    feat(clase-6): add literal object and list

--- 01-bases/src/typescript/BasicTypes.tsx
+++ 01-bases/src/typescript/BasicTypes.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const name:string = "victor";
-const age:number = 23;
-const isActive:boolean = false;
-  
+const name: string = "victor";
+const age: number = 23;
+const isActive: boolean = false;
+const names: string[] = ["Pedro", "Juan", "Julio"];
+const person = {
+  age: 32,
+  name: "victor",
+  address: {
+    country: "Colombia",
+    numberHouse: "22",
+  },
+};
+
 export const BasicTypes = () => {
-  
   return (
     <div>
       <p>{name}</p>
       <p>{age}</p>
-      <p>{isActive ? 'Activo' : 'No Activo'}</p>
-
+      <p>{isActive ? "Activo" : "No Activo"}</p>
+      <p>
+        <b>Lista: </b> {names.join(", ")}
+      </p>
+      <p>
+        <b>Literal Object: </b> {JSON.stringify(person, null, 2)}
+      </p>
     </div>
-  )
-}
+  );
+};
