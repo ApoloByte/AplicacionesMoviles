b0185916e7b8896f95b7327ba38b395625f0519e
Victor-P <victorpinto2304@gmail.com>
2025-03-21 08:30:13 -0500

    feat(clase-6): add constants
    add name, age and isActive constants

--- 01-bases/src/typescript/BasicTypes.tsx
+++ 01-bases/src/typescript/BasicTypes.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+const name:string = "victor";
+const age:number = 23;
+const isActive:boolean = false;
+  
 export const BasicTypes = () => {
+  
   return (
     <div>
-      
+      <p>{name}</p>
+      <p>{age}</p>
+      <p>{isActive ? 'Activo' : 'No Activo'}</p>
+
     </div>
   )
 }
