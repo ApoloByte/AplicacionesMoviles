afa46ee7433c6ca0071089ac11f05668d5238943
Victor-P <victorpinto2304@gmail.com>
2025-03-21 08:20:19 -0500

    feat(bases): add BasicTypes file

--- 01-bases/src/typescript/BasicTypes.tsx
+++ 01-bases/src/typescript/BasicTypes.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 
 export const BasicTypes = () => {
-    const name:string = 'Victor';
-    const age: number = 32;
-    const isActive: boolean = true;
   return (
-    <>
-
-    </>
+    <div>
+      
+    </div>
   )
 }
